2400e2034a4f0cc44dead25ffe785a305869a9f4
juniormena <juniormelpelotero@hotmail.com>
2021-10-12 01:21:40 -0400

    ...

--- server/src/utils/helpers.js
+++ server/src/utils/helpers.js
@@ -19,27 +19,38 @@ const isEmpty = (string) => {
   return true;
 };
 
-const validateDominicanID = (cedula) => {
-  let vnTotal = 0;
-  let pLongCed = cedula.trim().length;
-  const digitoMult = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1];
-
-  if (pLongCed < 11 || pLongCed > 11) {
-    throw new Error("Cedula no tiene un formato correcto");
+const validateDominicanID = (ced) => {
+  var c = ced.replace(/-/g, "");
+  var cedula = c.substr(0, c.length - 1);
+  var verificador = c.substr(c.length - 1, 1);
+  var suma = 0;
+  var cedulaValida = false;
+  if (ced.length < 11) {
+    throw new Error("La cedula no es correcta");
   }
-
-  for (let vDig = 1; vDig <= pLongCed; vDig++) {
-    let vCalculo =
-      parseInt(vcCedula.substring(vDig - 1, 1)) * digitoMult[vDig - 1];
-    if (vCalculo < 10) vnTotal += vCalculo;
-    else
-      vnTotal +=
-        parseInt(vCalculo.toString().substring(0, 1)) +
-        parseInt(vCalculo.toString().substring(1, 1));
+  for (i = 0; i < cedula.length; i++) {
+    mod = "";
+    if (i % 2 == 0) {
+      mod = 1;
+    } else {
+      mod = 2;
+    }
+    let res = cedula.substr(i, 1) * mod;
+    if (res > 9) {
+      res = res.toString();
+      uno = res.substr(0, 1);
+      dos = res.substr(1, 1);
+      res = eval(uno) + eval(dos);
+    }
+    suma += eval(res);
   }
-
-  if (vnTotal % 10 == 0) return true;
-  else throw new Error("Cedula no tiene un formato correcto");
+  el_numero = (10 - (suma % 10)) % 10;
+  if (el_numero == verificador && cedula.substr(0, 3) != "000") {
+    cedulaValida = true;
+  } else {
+    throw new Error("La cedula no es correcta");
+  }
+  return cedulaValida;
 };
 
 module.exports = {
